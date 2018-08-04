79583f0042091df183d340815b1257f30ce899af
Laurent Bernabe <laurent.bernabe@gmail.com>
2018-08-04 17:24:31 +0200

    Organizing vectors into groups and path, and also cascading drawing styles.

--- lib/main.dart
+++ lib/main.dart
@@ -67,19 +67,34 @@ class MyChessVector extends StatelessWidget {
       width: size,
       height: size,
       child: new CustomPaint(
-        painter: new VectorImagePainter(<VectorImagePathGroup>[
-          VectorImagePathGroup(path:"M 22,10 C 32.5,11 38.5,18 38,39"
-              " L 15,39 C 15,30 25,32.5 23,18"),
-          VectorImagePathGroup(path:"M 24,18 C 24.38,20.91 18.45,25.37"
-              " 16,27 C 13,29 13.18,31.34 11,31 C "
-          "9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30"
-              " 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C"
-              " 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L"
-              " 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10"),
-          VectorImagePathGroup(path: "M 9.5 25.5 A 0.5 0.5 0 1 1"
-              " 8.5,25.5 A 0.5 0.5 0 1 1 9.5 25.5 z", fillColor: Colors.black),
-          VectorImagePathGroup(path: "M 15 15.5 A 0.5 1.5 0 1 1"
-              "  14,15.5 A 0.5 1.5 0 1 1  15 15.5 z", fillColor: Colors.black)
+        painter: new VectorImagePainter(<VectorDrawableElement>[
+          VectorImageGroup(
+            children: <VectorDrawableElement>[
+              VectorImagePathDefinition(path:"M 22,10 C 32.5,11 38.5,18 38,39"
+                  " L 15,39 C 15,30 25,32.5 23,18",
+                drawingParameters: DrawingParameters(fillColor: Colors.white)
+              ),
+              VectorImagePathDefinition(path:"M 24,18 C 24.38,20.91 18.45,25.37"
+                  " 16,27 C 13,29 13.18,31.34 11,31 C "
+                  "9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30"
+                  " 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C"
+                  " 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L"
+                  " 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10",
+                  drawingParameters: DrawingParameters(fillColor: Colors.white)
+              ),
+              VectorImagePathDefinition(path: "M 9.5 25.5 A 0.5 0.5 0 1 1"
+                  " 8.5,25.5 A 0.5 0.5 0 1 1 9.5 25.5 z",
+                drawingParameters: DrawingParameters(fillColor: Colors.black)),
+              VectorImagePathDefinition(path: "M 15 15.5 A 0.5 1.5 0 1 1"
+                  "  14,15.5 A 0.5 1.5 0 1 1  15 15.5 z",
+                  drawingParameters: DrawingParameters(fillColor: Colors.black))
+            ],
+            drawingParameters: DrawingParameters(
+              fillColor: null,
+              strokeColor: Colors.black,
+              strokeWidth: 1.5,
+            )
+          )
         ]),
       ),
     );
--- lib/vector_image.dart
+++ lib/vector_image.dart
@@ -5,32 +5,15 @@ import 'package:meta/meta.dart';
 import 'package:tuple/tuple.dart';
 
 class VectorImagePainter extends CustomPainter {
-  List<VectorImagePathGroup> vectorDefinition;
+  List<VectorDrawableElement> vectorDefinition;
 
   VectorImagePainter(this.vectorDefinition);
 
   @override
   void paint(Canvas canvas, Size size) {
-    vectorDefinition.forEach((VectorImagePathGroup pathDefinition) {
-
-      var commonPath = new Path();
-      pathDefinition.pathElements.forEach((PathElement element) {
-        element.addToPath(commonPath);
-      });
-
-      var strokePathPaint = new Paint()
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 1.0
-        ..color = pathDefinition.strokeColor;
-      canvas.drawPath(commonPath, strokePathPaint);
-
-      var fillPathPaint = new Paint()
-        ..style = PaintingStyle.fill
-        ..strokeWidth = 1.0
-        ..color = pathDefinition.fillColor;
-      canvas.drawPath(commonPath, fillPathPaint);
+    vectorDefinition.forEach((VectorDrawableElement vectorElement) {
+      vectorElement.paintIntoCanvas(canvas, vectorElement.drawingParameters);
     });
-
   }
 
   @override
@@ -39,17 +22,94 @@ class VectorImagePainter extends CustomPainter {
   }
 }
 
+DrawingParameters mergeDrawingParameters(DrawingParameters thisDrawingParameters,
+    DrawingParameters parentDrawingParameters) {
+  DrawingParameters usedDrawingParameters = DrawingParameters(
+    fillColor: thisDrawingParameters.fillColor ?? parentDrawingParameters.fillColor,
+    strokeColor: thisDrawingParameters.strokeColor ?? parentDrawingParameters.strokeColor,
+    strokeWidth: thisDrawingParameters.strokeWidth ?? parentDrawingParameters.strokeWidth,
+  );
+  return usedDrawingParameters;
+}
 
-class VectorImagePathGroup {
-  List<PathElement> pathElements;
+class DrawingParameters {
   Color fillColor;
   Color strokeColor;
+  double strokeWidth;
 
-  VectorImagePathGroup({
+  DrawingParameters(
+      {this.fillColor, this.strokeColor, this.strokeWidth});
+
+  @override
+  String toString() {
+    return "DrawingParameters("
+        "fillColor = $fillColor,"
+        "strokeColor = $strokeColor,"
+        "strokeWidth = $strokeWidth,"
+        ")";
+  }
+}
+
+abstract class VectorDrawableElement {
+  DrawingParameters drawingParameters;
+
+  VectorDrawableElement(this.drawingParameters);
+
+  void paintIntoCanvas(Canvas targetCanvas, DrawingParameters parentDrawingParameters);
+}
+
+class VectorImageGroup extends VectorDrawableElement {
+  List<VectorDrawableElement> children;
+
+  VectorImageGroup({
+    this.children,
+    DrawingParameters drawingParameters,
+  }) : super(drawingParameters);
+
+  @override
+  void paintIntoCanvas(Canvas targetCanvas, DrawingParameters parentDrawingParameters) {
+
+    DrawingParameters usedDrawingParameters =
+      mergeDrawingParameters(drawingParameters, parentDrawingParameters);
+
+    children.forEach((VectorDrawableElement currentChild){
+      currentChild.paintIntoCanvas(targetCanvas, usedDrawingParameters);
+    });
+  }
+
+}
+
+class VectorImagePathDefinition extends VectorDrawableElement {
+  List<PathElement> pathElements;
+
+  VectorImagePathDefinition({
     String path,
-    this.fillColor = Colors.white,
-    this.strokeColor = Colors.black
-  }) : pathElements = parsePath(path);
+    DrawingParameters drawingParameters,
+  })  : pathElements = parsePath(path),
+        super(drawingParameters);
+
+  @override
+  void paintIntoCanvas(Canvas targetCanvas, DrawingParameters parentDrawingParameters) {
+
+    DrawingParameters usedDrawingParameters =
+      mergeDrawingParameters(drawingParameters, parentDrawingParameters);
+
+    var commonPath = new Path();
+    pathElements.forEach((PathElement element) {
+      element.addToPath(commonPath);
+    });
+
+    var strokePathPaint = new Paint()
+      ..style = PaintingStyle.stroke
+      ..color = usedDrawingParameters.strokeColor
+      ..strokeWidth = usedDrawingParameters.strokeWidth;
+    targetCanvas.drawPath(commonPath, strokePathPaint);
+
+    var fillPathPaint = new Paint()
+      ..style = PaintingStyle.fill
+      ..color = usedDrawingParameters.fillColor;
+    targetCanvas.drawPath(commonPath, fillPathPaint);
+  }
 }
 
 abstract class PathElement {
@@ -57,74 +117,63 @@ abstract class PathElement {
 }
 
 class MoveElement extends PathElement {
-
   bool relative;
   Offset moveParams;
 
-  MoveElement({
-    @required this.moveParams,
-    @required this.relative
-  });
+  MoveElement({@required this.moveParams, @required this.relative});
 
   @override // ignore: missing_function_body
   void addToPath(Path path) {
     if (relative) {
       path.relativeMoveTo(moveParams.dx, moveParams.dy);
-    }
-    else {
+    } else {
       path.moveTo(moveParams.dx, moveParams.dy);
     }
   }
 
-  @override String toString() {
+  @override
+  String toString() {
     return "MoveElement("
         "relative = $relative, "
         "moveParams = $moveParams"
         ")";
   }
-
 }
 
 class CloseElement extends PathElement {
-
   @override
   void addToPath(Path path) {
     path.close();
   }
 
-  @override String toString() {
+  @override
+  String toString() {
     return "CloseElement()";
   }
-
 }
 
 class LineElement extends PathElement {
-
   bool relative;
   Offset lineParams;
 
-  LineElement({
-    @required this.lineParams,
-    @required this.relative
-  });
+  LineElement({@required this.lineParams, @required this.relative});
 
   @override
   void addToPath(Path path) {
     if (relative) {
       path.relativeLineTo(lineParams.dx, lineParams.dy);
-    }
-    else {
+    } else {
       path.lineTo(lineParams.dx, lineParams.dy);
     }
   }
 
-  @override String toString() {
+  @override
+  String toString() {
     return "LineElement("
         "relative = $relative, "
         "lineParams = $lineParams"
         ")";
   }
-
 }
 
 class CubicCurveElement extends PathElement {
@@ -144,21 +193,25 @@ class CubicCurveElement extends PathElement {
   void addToPath(Path path) {
     if (relative) {
       path.relativeCubicTo(
-          firstControlPoint.dx, firstControlPoint.dy,
-          secondControlPoint.dx, secondControlPoint.dy,
-          endPoint.dx, endPoint.dy
-      );
-    }
-    else {
+          firstControlPoint.dx,
+          firstControlPoint.dy,
+          secondControlPoint.dx,
+          secondControlPoint.dy,
+          endPoint.dx,
+          endPoint.dy);
+    } else {
       path.cubicTo(
-          firstControlPoint.dx, firstControlPoint.dy,
-          secondControlPoint.dx, secondControlPoint.dy,
-          endPoint.dx, endPoint.dy
-      );
+          firstControlPoint.dx,
+          firstControlPoint.dy,
+          secondControlPoint.dx,
+          secondControlPoint.dy,
+          endPoint.dx,
+          endPoint.dy);
     }
   }
 
-  @override String toString() {
+  @override
+  String toString() {
     return "CubicCurveElement("
         "relative = $relative, "
         "firstControlPoint = $firstControlPoint,"
@@ -174,12 +227,11 @@ class ArcElement extends PathElement {
   double xAxisRotation;
   Offset center;
 
-  ArcElement({
-    @required this.relative,
-    @required this.radius,
-    @required this.xAxisRotation,
-    @required this.center
-  });
+  ArcElement(
+      {@required this.relative,
+      @required this.radius,
+      @required this.xAxisRotation,
+      @required this.center});
 
   @override
   void addToPath(Path path) {
@@ -189,8 +241,7 @@ class ArcElement extends PathElement {
         rotation: xAxisRotation,
         radius: Radius.elliptical(radius.dx, radius.dy),
       );
-    }
-    else {
+    } else {
       path.arcToPoint(
         center,
         rotation: xAxisRotation,
@@ -199,8 +250,8 @@ class ArcElement extends PathElement {
     }
   }
 
-
-  @override String toString() {
+  @override
+  String toString() {
     return "ArcElement("
         "relative = $relative, "
         "radius = $radius,"
@@ -208,78 +259,62 @@ class ArcElement extends PathElement {
         "center = $center"
         ")";
   }
-
 }
 
-List<List<PathElement>> parsePathList(List<String> pathList){
+List<List<PathElement>> parsePathList(List<String> pathList) {
   return pathList.map((var currentPathStr) {
     return parsePath(currentPathStr);
   }).toList();
 }
 
 List<PathElement> parsePath(String pathStr) {
-  Tuple2<PathElement, String> interpretCommand(RegExp commandRegex, String input) {
+  Tuple2<PathElement, String> interpretCommand(
+      RegExp commandRegex, String input) {
     var commandInterpretation = commandRegex.firstMatch(input);
     if (commandInterpretation == null) return null;
 
     var commandType = commandInterpretation.group(1);
     var relativeCommand = commandType.toLowerCase() == commandType;
-    switch(commandType){
+    switch (commandType) {
       case 'M':
       case 'm':
         var element = MoveElement(
-          relative: relativeCommand,
-          moveParams: Offset(
-              double.parse(commandInterpretation.group(2)),
-              double.parse(commandInterpretation.group(3))
-          )
-        );
+            relative: relativeCommand,
+            moveParams: Offset(double.parse(commandInterpretation.group(2)),
+                double.parse(commandInterpretation.group(3))));
         var remainingPathStr = input.substring(commandInterpretation.end);
         return Tuple2<PathElement, String>(element, remainingPathStr);
       case 'L':
       case 'l':
         var element = LineElement(
-          relative: relativeCommand,
-          lineParams: Offset(
-            double.parse(commandInterpretation.group(2)),
-            double.parse(commandInterpretation.group(3))
-          )
-        );
+            relative: relativeCommand,
+            lineParams: Offset(double.parse(commandInterpretation.group(2)),
+                double.parse(commandInterpretation.group(3))));
         var remainingPathStr = input.substring(commandInterpretation.end);
         return Tuple2<PathElement, String>(element, remainingPathStr);
       case 'c':
       case 'C':
         var element = CubicCurveElement(
-          relative: relativeCommand,
-          firstControlPoint: Offset(
-            double.parse(commandInterpretation.group(2)),
-            double.parse(commandInterpretation.group(3))
-          ),
-          secondControlPoint: Offset(
-            double.parse(commandInterpretation.group(4)),
-            double.parse(commandInterpretation.group(5))
-          ),
-          endPoint: Offset(
-            double.parse(commandInterpretation.group(6)),
-            double.parse(commandInterpretation.group(7))
-          )
-        );
+            relative: relativeCommand,
+            firstControlPoint: Offset(
+                double.parse(commandInterpretation.group(2)),
+                double.parse(commandInterpretation.group(3))),
+            secondControlPoint: Offset(
+                double.parse(commandInterpretation.group(4)),
+                double.parse(commandInterpretation.group(5))),
+            endPoint: Offset(double.parse(commandInterpretation.group(6)),
+                double.parse(commandInterpretation.group(7))));
         var remainingPathStr = input.substring(commandInterpretation.end);
         return Tuple2<PathElement, String>(element, remainingPathStr);
       case 'a':
       case 'A':
         var element = ArcElement(
-          relative: relativeCommand,
-          radius: Offset(
-            double.parse(commandInterpretation.group(2)),
-            double.parse(commandInterpretation.group(3))
-          ),
-          center: Offset(
-            double.parse(commandInterpretation.group(7)),
-            double.parse(commandInterpretation.group(8))
-          ),
-          xAxisRotation: double.parse(commandInterpretation.group(4))
-        );
+            relative: relativeCommand,
+            radius: Offset(double.parse(commandInterpretation.group(2)),
+                double.parse(commandInterpretation.group(3))),
+            center: Offset(double.parse(commandInterpretation.group(7)),
+                double.parse(commandInterpretation.group(8))),
+            xAxisRotation: double.parse(commandInterpretation.group(4)));
         var remainingPathStr = input.substring(commandInterpretation.end);
         return Tuple2<PathElement, String>(element, remainingPathStr);
       case 'z':
@@ -290,20 +325,23 @@ List<PathElement> parsePath(String pathStr) {
     }
     return null;
   }
-  
+
   String valueFormat = r"(\d+(?:\.\d+)?)";
   String separatorFormat = r"(?:\s+|,)";
 
-  var moveRegex = RegExp("^(M|m)$separatorFormat$valueFormat$separatorFormat$valueFormat");
-  var lineRegex = RegExp("^(L|l)$separatorFormat$valueFormat$separatorFormat$valueFormat");
-  var cubicCurveRegex = RegExp("^(C|c)$separatorFormat$valueFormat$separatorFormat"+
-      "$valueFormat$separatorFormat$valueFormat$separatorFormat$valueFormat$separatorFormat"+
+  var moveRegex =
+      RegExp("^(M|m)$separatorFormat$valueFormat$separatorFormat$valueFormat");
+  var lineRegex =
+      RegExp("^(L|l)$separatorFormat$valueFormat$separatorFormat$valueFormat");
+  var cubicCurveRegex = RegExp(
+      "^(C|c)$separatorFormat$valueFormat$separatorFormat" +
+          "$valueFormat$separatorFormat$valueFormat$separatorFormat$valueFormat$separatorFormat" +
           "$valueFormat$separatorFormat$valueFormat");
-  var arcRegex = RegExp("^(A|a)$separatorFormat$valueFormat$separatorFormat$valueFormat"+
-      "$separatorFormat$valueFormat$separatorFormat$valueFormat$separatorFormat$valueFormat"+
+  var arcRegex = RegExp(
+      "^(A|a)$separatorFormat$valueFormat$separatorFormat$valueFormat" +
+          "$separatorFormat$valueFormat$separatorFormat$valueFormat$separatorFormat$valueFormat" +
           "$separatorFormat$valueFormat$separatorFormat$valueFormat");
   var closeRegex = RegExp("^(z)");
-
 
   var elementsToReturn = <PathElement>[];
   var remainingPath = pathStr.trim();
@@ -311,31 +349,28 @@ List<PathElement> parsePath(String pathStr) {
   while (remainingPath.isNotEmpty) {
     var moveElementTuple = interpretCommand(moveRegex, remainingPath);
     var lineElementTuple = interpretCommand(lineRegex, remainingPath);
-    var cubicCurveElementTuple = interpretCommand(cubicCurveRegex, remainingPath);
+    var cubicCurveElementTuple =
+        interpretCommand(cubicCurveRegex, remainingPath);
     var arcElementTuple = interpretCommand(arcRegex, remainingPath);
     var closeElementTuple = interpretCommand(closeRegex, remainingPath);
 
     if (moveElementTuple != null) {
       elementsToReturn.add(moveElementTuple.item1);
       remainingPath = moveElementTuple.item2.trim();
-    }
-    else if (lineElementTuple != null) {
+    } else if (lineElementTuple != null) {
       elementsToReturn.add(lineElementTuple.item1);
       remainingPath = lineElementTuple.item2.trim();
-    }
-    else if (cubicCurveElementTuple != null) {
+    } else if (cubicCurveElementTuple != null) {
       elementsToReturn.add(cubicCurveElementTuple.item1);
       remainingPath = cubicCurveElementTuple.item2.trim();
-    }
-    else if (arcElementTuple != null) {
+    } else if (arcElementTuple != null) {
       elementsToReturn.add(arcElementTuple.item1);
       remainingPath = arcElementTuple.item2.trim();
-    }
-    else if (closeElementTuple != null) {
+    } else if (closeElementTuple != null) {
       elementsToReturn.add(closeElementTuple.item1);
       remainingPath = closeElementTuple.item2.trim();
-    }
-    else throw "Unrecognized path in $remainingPath !";
+    } else
+      throw "Unrecognized path in $remainingPath !";
   }
 
   return elementsToReturn;
